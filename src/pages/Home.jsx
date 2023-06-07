d4d114d6181b3d5296b6c51014f5df413f98be10
Alfe3n <115562186+Alfe3n@users.noreply.github.com>
2023-06-07 17:30:17 +0530

    All basic layout done To do work on background image implement form make it responsive

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,13 @@
 import { React, useEffect, useState } from "react";
 import axios from "axios";
-import { useDispatch } from "react-redux";
-import { selectMovie } from "../Slice";
 import { Link } from "react-router-dom";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination, Navigation } from "swiper";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 function Home() {
-  // const dispatch = useDispatch();
-  // function handleMovieClick(movie) {
-  //   dispatch(selectMovie(movie));
-  // }
   const [movieList, setMovieList] = useState([]);
   useEffect(() => {
     axios
@@ -24,22 +24,54 @@ function Home() {
   return (
     <div className="home-container">
       <div className="nav-container"></div>
-      <div className="image-banner">
-        <img src="https://static.tvmaze.com/uploads/images/medium_portrait/413/1034988.jpg"></img>
-      </div>
+      {movieList ? (
+        <Swiper
+          spaceBetween={30}
+          centeredSlides={true}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          modules={[Autoplay, Pagination, Navigation]}
+          className="mySwiper"
+        >
+          {movieList.map((movie) => {
+            return (
+              <SwiperSlide
+                className="mix-blend-overlay"
+                style={{ backgroundImage: `url(${movie.show.image.original})` }}
+              >
+                <div className="flex items-center justify-around ">
+                  <img src={movie.show.image.medium}></img>
+                  <p className="text-4xl font-bold">{movie.show.name}</p>
+                </div>
+              </SwiperSlide>
+            );
+          })}
+        </Swiper>
+      ) : (
+        <>
+          <h2>hello</h2>
+        </>
+      )}
+
       <div className="filter-container"></div>
-      <div className=" cards-container">
-        <h2>Recommended movies</h2>
+      <div className="m-10 cards-container">
+        <h2 className="mb-10 text-2xl">Recommended movies</h2>
         <div className="flex flex-wrap gap-x-4 gap-y-6">
           {movieList.map((movie) => {
-            // console.log(movie.show.averageRuntime);
             return (
               <Link to={`bookmovie/${movie.show.id}`}>
-                <div
-                  key={movie.show.id}
-                  // onClick={() => handleMovieClick(movie)}
-                >
-                  <img src={movie.show.image.medium} alt="movie poster" />
+                <div key={movie.show.id}>
+                  <img
+                    src={movie.show.image.medium}
+                    alt="movie poster"
+                    className="rounded-lg"
+                  />
                   <p>{movie.show.name}</p>
                   <div>
                     {movie.show.genres.map((genre) => {
